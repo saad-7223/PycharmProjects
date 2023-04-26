7c0989e1dd579e0070226264b2d09264a7afa660
saad-7223 <saadsyed837@gmail.com>
2023-04-26 20:21:41 +0530

    initializing a highscore attribute

--- snake_game/scorebored.py
+++ snake_game/scorebored.py
@@ -8,20 +8,28 @@ class Scorebored(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high = 0
         self.color("White")
         self.pu()
         self.goto(0, 270)
-        self.write(f"Score : {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score : {self.score} Highscore :{self.high}", align=ALIGNMENT, font=FONT)
         self.hideturtle()
 
     def update_scorebored(self):
-        self.write(f"Score : {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score :{self.score} Highscore :{self.high}", align=ALIGNMENT, font=FONT)
 
     def scored(self):
         self.score += 1
         self.clear()
         self.update_scorebored()
+        self.Highscore()
 
     def gameover(self):
         self.goto(0, 0)
         self.write(f"GAME OVER", align="center", font=FONT)
+
+    def Highscore(self):
+        if self.score > self.high:
+            self.high = self.score
+        else:
+            self.high = self.high
