e4ea35b241f79e04204a489e308ee3b5af9368f1
saad-7223 <saadsyed837@gmail.com>
2023-04-28 00:12:51 +0530

    trying new stuff

--- learn_files/main.py
+++ learn_files/main.py
@@ -4,3 +4,9 @@ with open("my_file.txt", mode="r") as file:
     contents = file.read()
     print(contents)
 file.close()
+# import numpy as np
+#
+# arr = np.array([[1, 2, 3], [4, 5, 6]])
+#
+# print("Original array : \n", arr)
+# print("Transpose array : \n", arr.T)
