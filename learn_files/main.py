51e2c5135eb6e969ca767adbb9fadb1f21703e71
saad-7223 <saadsyed837@gmail.com>
2023-04-30 01:35:31 +0530

    learning pandas

--- learn_files/main.py
+++ learn_files/main.py
@@ -1,4 +1,12 @@
-with open("", mode="r") as file:
-    contents = file.read()
-    print(contents)
-file.close()
+# import csv
+#
+# temperature = []
+#
+# with open("weather_data.csv") as file:
+#     data = csv.reader(file)
+#     for row in data:
+#         if row[1]!= "temp":
+#             temperature.append(row[1])
+# print(temperature)
+
+import pandas
