b4ae0fde0aa7597a7ae983a26cb040c69600b94d
saad-7223 <saadsyed837@gmail.com>
2023-04-30 01:00:40 +0530

    file task completed state #automation

--- Mail+Merge+Project+Start/Mail Merge Project Start/main.py
+++ Mail+Merge+Project+Start/Mail Merge Project Start/main.py
@@ -1,11 +1,12 @@
-# for each name in invited_names.txt
-# Replace the [name] placeholder with the actual name.
-# Save the letters in the folder "ReadyToSend".
-
-# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+placeholder = "[name]"
+counter = 0
+with open("../Mail Merge Project Start/Input/Names/invited_names.txt", mode="r") as names_file:
+    names = names_file.readlines()
 with open("../Mail Merge Project Start/Input/Letters/starting_letter.txt", mode="r") as file:
-    letter = file.readlines()
-
-
+    letter = file.read()
+    for name in names:
+        new_name = name.strip()
+        new_letter = letter.replace(placeholder, new_name)
+        with open(f"./Output/ReadyToSend/letter{counter}.txt", mode="w") as f:
+            f.write(new_letter)
+        counter += 1
