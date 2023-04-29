ba697896f621d01a85ec028d7a54668bf06c1189
saad-7223 <saadsyed837@gmail.com>
2023-04-30 01:04:39 +0530

    small change

--- Mail+Merge+Project+Start/Mail Merge Project Start/main.py
+++ Mail+Merge+Project+Start/Mail Merge Project Start/main.py
@@ -8,6 +8,6 @@ with open("../Mail Merge Project Start/Input/Letters/starting_letter.txt", mode=
     for name in names:
         new_name = name.strip()
         new_letter = letter.replace(placeholder, new_name)
-        with open(f"./Output/ReadyToSend/letter{new_name}.txt", mode="w") as f:
+        with open(f"./Output/ReadyToSend/{new_name}_letter.txt", mode="w") as f:
             f.write(new_letter)
 
