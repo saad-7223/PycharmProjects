e89d1d46434527dd7e4584a9a5766a6eaefd8c6f
saad-7223 <saadsyed837@gmail.com>
2023-04-29 10:46:34 +0530

    new update : made the highscore attribute

--- snake_game/scorebored.py
+++ snake_game/scorebored.py
@@ -2,21 +2,22 @@ from turtle import Turtle
 
 ALIGNMENT = "center"
 FONT = ("courier", 20, "normal")
-
+with open("high_score.txt", mode="r") as file:
+    H = file.read()
 
 class Scorebored(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high = 0
+        # self.high = 0
         self.color("White")
         self.pu()
         self.goto(0, 270)
-        self.write(f"Score : {self.score} Highscore :{self.high}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score : {self.score} Highscore :{H}", align=ALIGNMENT, font=FONT)
         self.hideturtle()
 
     def update_scorebored(self):
-        self.write(f"Score :{self.score} Highscore :{self.high}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score :{self.score} Highscore :{H}", align=ALIGNMENT, font=FONT)
 
     def scored(self):
         self.score += 1
@@ -24,13 +25,14 @@ class Scorebored(Turtle):
         self.Highscore()
         self.update_scorebored()
 
-
     def gameover(self):
         self.goto(0, 0)
         self.write(f"GAME OVER", align="center", font=FONT)
 
     def Highscore(self):
-        if self.score > self.high:
-            self.high = self.score
+        if self.score > int(H):
+            with open("high_score.txt", mode="w") as f:
+                f.write(f"{self.score}")
         else:
-            self.high = self.high
+            with open("high_score.txt", mode="w") as f:
+                f.write(f"{H}")
