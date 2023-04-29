0c7577b455c966635a309f4809d94ad540425f1e
saad-7223 <saadsyed837@gmail.com>
2023-04-29 15:40:51 +0530

    file task initial state practice

--- learn_files/main.py
+++ learn_files/main.py
@@ -1,12 +1,4 @@
-file = open("my_file.txt", mode="a")
-file.write("but still he is my cute lil brother ..:)")
-with open("my_file.txt", mode="r") as file:
+with open("", mode="r") as file:
     contents = file.read()
     print(contents)
 file.close()
-# import numpy as np
-#
-# arr = np.array([[1, 2, 3], [4, 5, 6]])
-#
-# print("Original array : \n", arr)
-# print("Transpose array : \n", arr.T)
