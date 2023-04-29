ca8a790a7b3817c1fa21b1c18ec29d19d7e646a5
saad-7223 <saadsyed837@gmail.com>
2023-04-30 01:02:23 +0530

    small change

--- Mail+Merge+Project+Start/Mail Merge Project Start/main.py
+++ Mail+Merge+Project+Start/Mail Merge Project Start/main.py
@@ -1,12 +1,13 @@
 placeholder = "[name]"
-counter = 0
+
 with open("../Mail Merge Project Start/Input/Names/invited_names.txt", mode="r") as names_file:
     names = names_file.readlines()
+
 with open("../Mail Merge Project Start/Input/Letters/starting_letter.txt", mode="r") as file:
     letter = file.read()
     for name in names:
         new_name = name.strip()
         new_letter = letter.replace(placeholder, new_name)
-        with open(f"./Output/ReadyToSend/letter{counter}.txt", mode="w") as f:
+        with open(f"./Output/ReadyToSend/letter{new_name}.txt", mode="w") as f:
             f.write(new_letter)
-        counter += 1
+
