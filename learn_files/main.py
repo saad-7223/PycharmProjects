04bf4520c760aac0cd1a439950dfb54ad1aa5a54
saad-7223 <saadsyed837@gmail.com>
2023-05-02 19:51:50 +0530

    cleaning code .. ;)

--- learn_files/main.py
+++ learn_files/main.py
@@ -1,26 +1,15 @@
-# import csv
-#
-# temperature = []
-#
-# with open("weather_data.csv") as file:
-#     data = csv.reader(file)
-#     for row in data:
-#         if row[1]!= "temp":
-#             temperature.append(row[1])
-# print(temperature)
-
 import pandas as pd
 
+# accessing data fom csv file
 data = pd.read_csv("weather_data.csv")
 temp = data['temp'].to_list()
-# print(data)
 
-# maximum = data['temp'].max()
-# print(maximum)
+# getting max temp fom data
+maximum = data['temp'].max()
 
+# coveting celsius data to fahrenheit
 friday = (data[data.day == 'Friday']).temp
 cal_to_fahrenheit = (friday * (9 / 5)) + 32
-# print(cal_to_fahrenheit)
 
 # creating dataframe from scratch
 student = {
