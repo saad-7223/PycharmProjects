a7ea31943dd9bccdcf797cdc6b9b65a6b8bf5033
saad-7223 <saadsyed837@gmail.com>
2023-05-02 19:46:33 +0530

    creating dataframe in pandas

--- learn_files/main.py
+++ learn_files/main.py
@@ -19,8 +19,14 @@ temp = data['temp'].to_list()
 # print(maximum)
 
 friday = (data[data.day == 'Friday']).temp
-cal_to_fahrenheit = (friday*(9/5))+32
-print(cal_to_fahrenheit)
+cal_to_fahrenheit = (friday * (9 / 5)) + 32
+# print(cal_to_fahrenheit)
 
-
+# creating dataframe from scratch
+student = {
+    "names": ['saad', 'saarif', 'tazkiya', 'ajmal'],
+    "marks": [25, 50, 60, 50]
+}
+student_data = pd.DataFrame(student)
+student_data.to_csv("student.csv")
 
