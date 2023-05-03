642a38e1fffdcd6a436e8c71db77b938ec86a94d
saad-7223 <saadsyed837@gmail.com>
2023-05-03 23:43:51 +0530

    small update

--- learn_files/main.py
+++ learn_files/main.py
@@ -14,7 +14,8 @@ cal_to_fahrenheit = (friday * (9 / 5)) + 32
 # creating dataframe from scratch
 student = {
     "names": ['saad', 'saarif', 'tazkiya', 'ajmal'],
-    "marks": [25, 50, 60, 50]
+    "marks": [25, 50, 60, 50],
+    "grade": ['c', 'b', 'a', 'b']
 }
 student_data = pd.DataFrame(student)
 student_data.to_csv("student.csv")
