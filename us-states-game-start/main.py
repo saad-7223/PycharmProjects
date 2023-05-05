53c0d3579c3adb92bad8b80d15b276f87312c39c
saad-7223 <saadsyed837@gmail.com>
2023-05-05 22:33:31 +0530

    trying the india map

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -3,20 +3,22 @@ import pandas as pd
 
 screen = turtle.Screen()
 screen.title("India States game")
-screen.setup(width=770, height=550)
-img = "blank_states_img.gif"
+# screen.setup(width=770, height=550)
+img = "india-outline-map.gif"
 screen.addshape(img)
 turtle.shape(img)
 
 data = pd.read_csv("36_states.csv")
 all_states = data.state.to_list()
 
+
 def get_mouse_click_co_or(x, y):
     print(x, y)
+
 
 ans = screen.textinput(title="Guess the state", prompt="state the name ?")
 turtle.onscreenclick(get_mouse_click_co_or)
 
 if ans in all_states:
-
+    pass
 screen.mainloop()
