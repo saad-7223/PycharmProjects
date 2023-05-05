395d300ac9851ed981c1b7bd493825b00466e692
saad-7223 <saadsyed837@gmail.com>
2023-05-06 00:00:23 +0530

    task : small add ons

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -1,8 +1,8 @@
 import turtle
 import pandas as pd
 
+guessed_state = []
 screen = turtle.Screen()
-screen.title("India States game")
 screen.setup(width=725, height=491)
 img = "blank_states_img.gif"
 screen.addshape(img)
@@ -10,8 +10,6 @@ turtle.shape(img)
 
 data = pd.read_csv("50_states.csv")
 all_states = data.state.to_list()
-guessed_state = []
-
 
 def get_mouse_click_co_or(x, y):
     print(x, y)
@@ -19,7 +17,9 @@ def get_mouse_click_co_or(x, y):
 
 while len(guessed_state) <= 50:
 
+    screen.title(f"{len(guessed_state)}/50 States covered")
     ans = screen.textinput(title="Guess the state", prompt="state the name ?")
+    ans = ans[0].upper() + ans[1:]
     turtle.onscreenclick(get_mouse_click_co_or)
 
     if ans in all_states:
