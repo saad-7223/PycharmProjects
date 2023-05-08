b50086badf20942f308cdacecdff7848f75b936c
saad-7223 <saadsyed837@gmail.com>
2023-05-08 18:48:39 +0530

    tested new co-ordinates

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -18,29 +18,29 @@ def get_mouse_click_co_or(x, y):
 
 turtle.onscreenclick(get_mouse_click_co_or)
 
-# while len(guessed_state) < 50:
-#
-#     screen.title(f"{len(guessed_state)}/50 States covered")
-#     ans = screen.textinput(title="Guess the state", prompt="state the name ?").title()
-#     ans = ans[0].upper() + ans[1:]
-#
-#     if ans in all_states:
-#         guessed_state.append(ans)
-#         t = turtle.Turtle()
-#         t.pu()
-#         t.hideturtle()
-#         state_data = data[data.state == ans]
-#         t.goto(int(state_data.x), int(state_data.y))
-#         t.write(f'{ans}', align='center', font=('courier', 9, 'bold'))
-#
-#     if ans == "Exit":
-#         not_learned = []
-#         for state in all_states:
-#             if state not in guessed_state:
-#                 not_learned.append(state)
-#         to_learn = pd.DataFrame(not_learned)
-#         to_learn.to_csv("states_to_learn")
-#         break
-turtle.mainloop()
+while len(guessed_state) < 50:
+
+    screen.title(f"{len(guessed_state)}/50 States covered")
+    ans = screen.textinput(title="Guess the state", prompt="state the name ?").title()
+    ans = ans[0].upper() + ans[1:]
+
+    if ans in all_states:
+        guessed_state.append(ans)
+        t = turtle.Turtle()
+        t.pu()
+        t.hideturtle()
+        state_data = data[data.state == ans]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(f'{ans}', align='center', font=('courier', 9, 'bold'))
+
+    if ans == "Exit":
+        not_learned = []
+        for state in all_states:
+            if state not in guessed_state:
+                not_learned.append(state)
+        to_learn = pd.DataFrame(not_learned)
+        to_learn.to_csv("states_to_learn")
+        break
+
 
 
