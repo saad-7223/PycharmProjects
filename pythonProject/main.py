516f78f063e7523866f4179556664c5fc3dad240
saad-7223 <saadsyed837@gmail.com>
2023-05-09 23:41:20 +0530

    difference b/w LIST COMPREHENSION and normal list

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,6 +1,16 @@
 # LIST COMPREHENSION
 # syntax : new_list = [new_item for item in numbers]
 # example
-numbers = [1,2,3]
-new_list = [n+1 for n in numbers]
+numbers = [1, 2, 3]
+new_list = [n + 1 for n in numbers]
 print(new_list)
+# the above code is using list comprehension
+
+# Now using the normal methord
+no = [1, 2, 3]
+new_l = []
+for number in no:
+    add = number + 1
+    new_l.append(add)
+print(new_l)
+# this is pretty lengthy ....
