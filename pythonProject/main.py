bb09983a07713444369fff44af5721bd1cee98e9
saad-7223 <saadsyed837@gmail.com>
2023-05-10 14:34:55 +0530

    task : new challenge

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,1 +1,4 @@
 # Learning about the dictionary comprehension
+# syntax : new_dict = {new_key:new_value for item in list}
+# syntax : new_dict = {new_key:new_value for (key,value) in dict.items()}
+# new challenge
