d392b9bdf294b0bb984db83be02a890f0f1c9419
saad-7223 <saadsyed837@gmail.com>
2023-05-11 21:04:20 +0530

    some clean ups

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -1,26 +1,24 @@
-import csv
-
 import pandas
-student_dict = {
-    "student": ["Angela", "James", "Lily"],
-    "score": [56, 76, 98]
-}
-
-# Looping through dictionaries:
-for (key, value) in student_dict.items():
-    # Access key and value
-    pass
-
-student_data_frame = pandas.DataFrame(student_dict)
-
-# Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    # Access index and row
-    # Access row.student or row.score
-    pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"],
+#     "score": [56, 76, 98]
+# }
+#
+# # Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     # Access key and value
+#     pass
+#
+# student_data_frame = pandas.DataFrame(student_dict)
+#
+# # Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     # Access index and row
+#     # Access row.student or row.score
+#     pass
+#
+# # Keyword Method with iterrows()
+# # {new_key:new_value for (index, row) in df.iterrows()}
 
 # TODO 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
