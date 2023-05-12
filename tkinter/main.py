4d081bf747116eb9567f6fe0522f758fe917283d
saad-7223 <saadsyed837@gmail.com>
2023-05-12 21:31:40 +0530

    to learn

--- tkinter/main.py
+++ tkinter/main.py
@@ -1,6 +1,7 @@
 # This is a sample Python script.
 import tkinter
 # task 1: learn basic of tkinter
+# task 1.1: tkinter widgets
 # task 2: learn advance arguments
 # task 2.1 : learn *args
 # task 2.2 ; learn **kwargs
