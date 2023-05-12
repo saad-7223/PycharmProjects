4e10195931c0b4608cda1dda3396e3a1898d4c7d
saad-7223 <saadsyed837@gmail.com>
2023-05-12 21:32:21 +0530

    to learn

--- tkinter/main.py
+++ tkinter/main.py
@@ -2,6 +2,7 @@
 import tkinter
 # task 1: learn basic of tkinter
 # task 1.1: tkinter widgets
+# task 1.2; tkinter layout managers
 # task 2: learn advance arguments
 # task 2.1 : learn *args
 # task 2.2 ; learn **kwargs
